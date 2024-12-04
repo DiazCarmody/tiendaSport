ea5c3f54adf746a1949b8ce2453abadfdec6876d
Fausto Díaz Carmody <0800fau@gmail.com>
2024-12-04 18:03:32 -0300

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+        <!-- --------------- FOOTER --------------- -->
+    <footer>
+    
+        <div class="footer-bottom">
+            <p>Copyright © 2024 Tienda Sport. Todos los derechos reservados.</p>
+        </div>
+    
+    </footer>
